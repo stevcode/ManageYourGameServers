bc6fac1cd213ce1ff0f4dc0bb96efb8a2e730ad9
stevcode <stev.code@gmail.com>
2022-01-08 17:13:41 -0500

    Small scaffolding.

---
+++ src/MYGS.Core/IGameServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYGS.Core;
+
+public interface IGameServer
+{
+    public void StartServer();
+    public void StopServer();
+    public void CheckForServerUpdate();
+    public void UpdateServer();
+}
