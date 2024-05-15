09ac757408e9c2ed56fdb1d912bd72cbadcee989
stevcode <stev.code@gmail.com>
2024-05-15 19:14:01 -0400

    Created method to download the dedicated server files for Sons of the Forest

--- src/ManageYourGameServers.Core/Services/SteamCmdService.cs
+++ src/ManageYourGameServers.Core/Services/SteamCmdService.cs
@@ -1,4 +1,5 @@
-﻿using System.IO.Compression;
+﻿using System.Diagnostics;
+using System.IO.Compression;
 
 namespace ManageYourGameServers.Core.Services;
 
@@ -28,7 +29,7 @@ public class SteamCmdService
     public async Task ExtractSteamCmdExecutableAsync()
     {
         var downloadPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "steamcmd.zip");
-        var extractionPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        var extractionPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "SteamCMD");
 
         try
         {
@@ -37,6 +38,58 @@ public class SteamCmdService
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
+        }
+    }
+
+    public async Task RunSteamCmdAsync()
+    {
+        var steamCmdFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "SteamCMD", "steamcmd.exe");
+        var serverDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "SoF");
+        var arguments = $"+login anonymous +force_install_dir \"{serverDirectoryPath}\" +app_update 2465200 validate +quit";
+
+        using var process = new Process();
+        process.StartInfo.FileName = steamCmdFilePath;
+        process.StartInfo.Arguments = arguments;
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.StartInfo.CreateNoWindow = true;
+
+        process.Start();
+
+        await ReadStreamAsync(process.StandardOutput, Console.Out);
+        await ReadStreamAsync(process.StandardError, Console.Error);
+
+        await process.WaitForExitAsync();
+    }
+
+    // TODO: Output of SteamCMD doesn't buffer properly. This will take some work to output in real time.
+    // This link outlines the specifics of the issue: https://github.com/ValveSoftware/Source-1-Games/issues/3658
+    private static async Task ReadStreamAsync(StreamReader reader, TextWriter writer)
+    {
+        var buffer = new char[4096];
+        int bytesRead;
+
+        while ((bytesRead = await reader.ReadAsync(buffer, 0, buffer.Length)) > 0)
+        {
+            var output = new string(buffer, 0, bytesRead);
+            await writer.WriteAsync(output);
+        }
+    }
+
+    private static void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(e.Data))
+        {
+            Console.WriteLine(e.Data);
+        }
+    }
+
+    private static void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(e.Data))
+        {
+            Console.WriteLine(e.Data);
         }
     }
 }
