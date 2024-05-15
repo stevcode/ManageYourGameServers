8afde91ca069a1a539960f5231d14d1de09aa03b
stevcode <stev.code@gmail.com>
2024-05-14 23:04:31 -0400

    Created SteamCmdService
    Correctly downloads steamcmd.zip from valve.

---
+++ src/ManageYourGameServers.Core/Services/SteamCmdService.cs
@@ -0,0 +1,25 @@
+﻿namespace ManageYourGameServers.Core.Services;
+
+public class SteamCmdService
+{
+    public async Task AcquireSteamCmdAsync()
+    {
+        var steamCmdUrl = "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip";
+        var downloadPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "steamcmd.zip");
+
+        using var client = new HttpClient();
+
+        try
+        {
+            using var response = await client.GetAsync(steamCmdUrl, HttpCompletionOption.ResponseHeadersRead);
+            using var stream = await response.Content.ReadAsStreamAsync();
+            using var fileStream = File.Create(downloadPath);
+
+            await stream.CopyToAsync(fileStream);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
