2e9ba9d8c01986b2d76c62793519927a186a7979
stevcode <stev.code@gmail.com>
2024-05-15 19:14:23 -0400

    Test for server file downloads.

--- src/ManageYourGameServers.CLI/Program.cs
+++ src/ManageYourGameServers.CLI/Program.cs
@@ -3,3 +3,7 @@
 var steamCmdService = new SteamCmdService();
 await steamCmdService.AcquireSteamCmdAsync();
 await steamCmdService.ExtractSteamCmdExecutableAsync();
+await steamCmdService.RunSteamCmdAsync();
+
+Console.WriteLine("Console Program Ended.");
+Console.ReadLine();
